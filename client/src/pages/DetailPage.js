0eade7d6d99629874e9de1e9ceeeef18d4bb8c8c
MariaSuslikova <79859761+MariaSuslikova@users.noreply.github.com>
2021-05-26 01:01:40 +0400

    Сделала страницу с деталями объявления

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {useParams} from "react-router-dom"
+import { AdDetailsCard } from '../components/AdDetailsCard'
+import { Loader } from '../components/Loader'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
 
 export const DetailPage = () => {
+    const {token} = useContext(AuthContext)
+    const {request, loading} = useHttp()
+    const [ad, setAd] = useState(null)
+    const adId = useParams().id
+
+    const getAd = useCallback(async () => {
+        try{
+            const fetched = await request(`/api/ad/${adId}`, "GET", null, {
+                Authorization: `Bearer ${token}`
+            })
+            setAd(fetched)
+        } catch (e){}
+    }, [token, adId, request])
+
+    useEffect(() => {
+        getAd()
+    }, [getAd])
+
+    if (loading) {
+        return <Loader/>
+    }
     return (
-        <div>
-            <h1>Detail Page</h1>
-        </div>
+        <>
+            { !loading && ad && <AdDetailsCard ad={ad} />}
+        </>
     )
 }
