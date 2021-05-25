46d7d387edce91d7f77b304c7459b091d56bd48c
MariaSuslikova <79859761+MariaSuslikova@users.noreply.github.com>
2021-05-26 01:12:05 +0400

    Cделала страницу с выводом всех объявлений в базе

--- client/src/pages/AdsPage.js
+++ client/src/pages/AdsPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { AdsList } from '../components/AdsList'
+import { Loader } from '../components/Loader'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
 
 export const AdsPage = () => {
+    const [ads, setAds] = useState([])
+    const {loading, request} = useHttp()
+    const {token} = useContext(AuthContext)
+
+    const fetchAds = useCallback(async () => {
+        try {
+            const fetched = await request("/api/ad", "GET", null, {
+                Authorization: `Bearer ${token}`
+            })
+            setAds(fetched)
+        } catch(e) {}
+    }, [token, request])
+
+    useEffect(() => {
+        fetchAds()
+    }, [fetchAds])
+
+    if (loading){
+        return <Loader/>
+    }
     return (
-        <div>
-            <h1>Ads Page</h1>
-        </div>
+        <>
+            {!loading && <AdsList ads={ads}/>}
+        </>
     )
 }
