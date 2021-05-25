24799f8634a9eb91c7526545ba3c3850c8020cc5
MariaSuslikova <79859761+MariaSuslikova@users.noreply.github.com>
2021-05-26 00:34:42 +0400

    Создала элемент загрузки страницы

---
+++ client/src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Loader = () => (
+    <div>
+        <div class="spinner-layer spinner-yellow">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+);
